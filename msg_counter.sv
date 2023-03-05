f41a7562f79c31d4f56b69a1c95d966f6c1ea5a6
nad <31370689+nadflop@users.noreply.github.com>
2023-03-06 00:44:33 +0800

    Modified logic for AXI-Slave, Added logic for data buffer. TODO: seperate blocks into other modules for better readability

--- msg_counter.sv
+++ msg_counter.sv
@@ -1,31 +1,52 @@
 module msg_counter 
 #(
-  parameter DATA_BYTES = 8,
   parameter NUM_COUNT_BITS = 16
 )
 (
 	input logic clk,
 	input logic rst,
-	input logic s_tvalid,
-	input logic s_tready,
-	output logic [NUM_COUNT_BITS-1:0] msg_length
+	input logic count_enable, //s_tvalid && s_tready
+	input logic clear, 
+	input logic [NUM_COUNT_BITS-1:0] rollover_val,
+	output logic [NUM_COUNT_BITS-1:0] msg_length,
+	output logic rollover_flag
 );
 
 	logic [NUM_COUNT_BITS-1:0] next_count;
+	logic flag;
 
 	always_ff @ (posedge clk, negedge rst) begin
 		if (!rst) begin
 			msg_length <= 0;
+			rollover_flag <= 0;
 		end
 		else begin
 			msg_length <= next_count;
+			rollover_flag <= flag;
 		end
 	end
 
+	//counter logic
 	always_comb begin
 		next_count = msg_length;
-		if (s_tvalid && s_tready) begin
-			next_count = msg_length + 1'b1;
+		flag = rollover_flag;
+		if (clear) begin
+			next_count = 0;
+			flag = 0;
+		end
+		if (count_enable) begin //if handshake happens, start the count
+			if (rollover_flag == 1'b1) begin 
+				next_count = 1;
+			end
+			else begin
+				next_count = msg_length + 1'b1;
+			end
+			if ((msg_length + 1 == rollover_val) || (msg_length == 1 && rollover_val == 1)) begin
+				flag = 1'b1;
+			end
+			else begin
+				flag = 1'b0;
+			end
 		end 
 	end
 
--- msg_parser.sv
+++ msg_parser.sv
@@ -1,101 +1,103 @@
 `timescale 1ns / 1ps
 module msg_parser #(
-  parameter MAX_MSG_BYTES = 32,
+  parameter MAX_MSG_BYTES = 32, //data width
   parameter DATA_BYTES = 8
 )(
+  //inputs, outputs from AXI Slave interface
   output logic        s_tready,
   input  logic        s_tvalid,
   input  logic        s_tlast,
   input  logic [8*DATA_BYTES-1:0] s_tdata,
   input  logic [7:0]  s_tkeep,
   input  logic        s_tuser, // Used as an error input signal, valid on tlast
-
+  //outputs from Data Buffer
   output logic                       msg_valid,   // High for one clock to output a message
   output logic [15:0]                msg_length,  // Length of the message
   output logic [8*MAX_MSG_BYTES-1:0] msg_data,    // Data with the LSB on [0]
   output logic                       msg_error,   // Output if issue with the message
-
   input  logic clk,
   input  logic rst
 );
 
-//state register
-typedef enum bit [2:0] {IDLE, MODIFY_DATA, WRITE} stateType;
-stateType current_state;
-stateType next_state;
-
-//find out if output bus is wider
+//variables used by both blocks//
 parameter upsizing = MAX_MSG_BYTES > DATA_BYTES;
+logic handshake = s_tvalid && s_tready;
 
-//TODO: add counter logic and maybe set the data here (since data needs to be set on clk cycle)
+//=====================AXI-ST SLAVE LOGIC=====================//
+//state register for AXI-ST slave FSM
+typedef enum bit [2:0] {IDLE, MODIFY_DATA, WRITE} stateType;
+stateType axi_current_state;
+stateType axi_next_state;
+
+//state assignments for AXI Slave interface
 always_ff @(posedge clk, negedge rst) begin
   if (!rst) begin
-    current_state <= IDLE;
+    axi_current_state <= IDLE;
   end
   else begin
-    current_state <= next_state;
+    axi_current_state <= axi_next_state;
   end
 end
 
-//logic for AXI slave FSM
+//next state logic for AXI Slave interface
 always_comb begin
-  next_state = current_state;
-  case(current_state)
+  axi_next_state = axi_current_state;
+  case(axi_current_state)
     IDLE: begin
-      if (s_tvalid && s_tready) begin
+      if (handshake) begin
         if (upsizing) begin
-          next_state = MODIFY_DATA;
+          axi_next_state = MODIFY_DATA;
         end
         else begin
-          next_state = WRITE;
+          axi_next_state = WRITE;
         end
       end
       else begin
-        next_state = current_state;
+        axi_next_state = axi_current_state;
       end
     end
     MODIFY_DATA: begin
-      if (s_tvalid && s_tready) begin
+      if (handshake) begin
         if (!s_tlast) begin
-          next_state = current_state;
+          axi_next_state = axi_current_state;
         end
         else begin
-          next_state = WRITE;
+          axi_next_state = WRITE;
         end
       end
       else begin
-        next_state = current_state;
+        axi_next_state = axi_current_state;
       end
     end
     WRITE: begin
-      //TODO: add logic for upsizing
-      if (s_tvalid && s_tready) begin
+      if (handshake) begin
         if (!s_tlast) begin
-          next_state = current_state;
+          axi_next_state = axi_current_state;
         end
         else begin
-          next_state = IDLE;
+          axi_next_state = IDLE;
         end
       end
       else begin
-        next_state = IDLE;
+        axi_next_state = IDLE;
       end
     end
   endcase
 end
 
-//combinational logic to generate tready signal
-//TODO: data buffer logic with tkeep and upsizing
+//output logic for AXI-ST Slave interface
 always_comb begin
-  case(current_state)
+  clear_count = 1'b0;
+  s_tready = 1'b0;
+  case(axi_current_state)
     IDLE: begin
       s_tready = 1'b1;
+      clear_count = 1'b1;
     end
     MODIFY_DATA: begin
       s_tready = 1'b1;
     end 
     WRITE: begin
-      //TODO: add logic for upsizing
       if (s_tlast) begin
         s_tready = 1'b0;
       end
@@ -103,6 +105,103 @@ always_comb begin
         s_tready = 1'b1;
       end
     end
+  endcase
+end
+
+//=====================DATA BUFFER LOGIC=====================//
+
+//input, output to the msg counter (part of component in data buffer)
+logic clear_msg_count;
+logic msg_ready; //tdata is ready to be copy to msg_data?
+
+//Data Counter
+msg_counter
+PKCT(
+  .clk(clk),
+	.rst(rst),
+	.count_enable(handshake),
+	.clear(clear_msg_count), 
+	.rollover_val(DATA_BYTES*8),
+	.msg_length(msg_length),
+	.rollover_flag(msg_ready)
+);
+
+//Data Controller
+//assumption: when upsizing, pad extra bits with '0'
+//assumption: tlast && tuser can be asserted anytime
+//state: idle/wait, read, store
+//idle/wait: output is all 0 or empty? should it retain prev val?
+//read: do the tkeep processing here (use for loop & if statement)
+//store: set msg_valid
+//error: tuser && tlast, set msg_error
+typedef enum bit [3:0] {WAIT, READ, ERROR, STORE} stateType;
+stateType data_ctrl_current_state;
+stateType data_ctrl_next_state;
+
+logic [8*MAX_MSG_BYTES-1:0] msg_temp = '0;
+
+//state assignments for Data Controller
+always_ff @(posedge clk, negedge rst) begin
+  if (!rst) begin
+    data_ctrl_current_state <= WAIT;
+    msg_data <= '0;
+  end
+  else begin
+    data_ctrl_current_state <= data_ctrl_next_state;
+    msg_data <= msg_temp;
+  end
+end
+
+//next state logic for Data Controller
+always_comb begin
+  data_ctrl_next_state = data_ctrl_current_state;
+  case(data_ctrl_current_state)
+    WAIT:
+      if (handshake) begin
+        if (s_tlast && s_tuser) begin
+          data_ctrl_next_state = ERROR;
+        end
+        else begin
+          data_ctrl_next_state = READ;
+        end
+      end
+      else begin
+        data_ctrl_next_state = data_ctrl_current_state;
+      end
+    READ:
+      if (handshake) begin
+        if (s_tlast && s_tuser) begin
+          data_ctrl_next_state = ERROR;
+        end
+        else begin
+          data_ctrl_next_state = data_ctrl_current_state;
+        end
+      end
+      else begin
+        data_ctrl_next_state = STORE;
+      end
+    STORE:
+      if (s_tlast && s_tuser) begin
+        data_ctrl_next_state = ERROR;
+      end
+      else begin
+        data_ctrl_next_state = WAIT;
+      end
+    ERROR:
+      data_ctrl_next_state =  WAIT;
+  endcase
+end
+
+//TODO: output logic for Data Controller
+always_comb begin
+  msg_temp = msg_data;
+  msg_valid = 1'b0;
+  msg_error = 1'b0;
+  case(data_ctrl_current_state)
+    WAIT:
+    READ:
+    STORE:
+    ERROR:
   endcase
 end
 
