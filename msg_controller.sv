ff8856173d4c2ed9da8a11cc851c8ad941dabd5c
nad <31370689+nadflop@users.noreply.github.com>
2023-03-09 01:49:08 +0800

    Update msg_controller.sv

--- msg_controller.sv
+++ msg_controller.sv
@@ -13,9 +13,11 @@ module msg_controller #(
 	input logic [TKEEP_WIDTH-1:0] s_tkeep,
 	input logic [8*DATA_BYTES-1:0] s_tdata,
   input logic upsizing,
+  input logic msg_full,
 	output logic [8*MAX_MSG_BYTES-1:0] msg_data,
 	output logic msg_valid,
-	output logic msg_error
+	output logic msg_error,
+	output logic count_en
 );
 //Data Controller
 //assumption: when upsizing, pad extra bits with '0'
@@ -30,6 +32,7 @@ dataStateType data_ctrl_current_state;
 dataStateType data_ctrl_next_state;
 
 logic [8*MAX_MSG_BYTES-1:0] msg_temp = '0;
+logic [8*MAX_MSG_BYTES-1:0] msg_temp_sync;
 integer i;
 
 //state assignments for Data Controller
@@ -37,10 +40,12 @@ always_ff @(posedge clk, negedge rst) begin
   if (!rst) begin
     data_ctrl_current_state <= WAIT;
     msg_data <= '0;
+    msg_temp_sync <= '0;
   end
   else begin
     data_ctrl_current_state <= data_ctrl_next_state;
     msg_data <= msg_temp;
+    msg_temp_sync <= msg_temp;
   end
 end
 
@@ -79,11 +84,12 @@ always_comb begin
   msg_temp = msg_data;
   msg_valid = 1'b0;
   msg_error = 1'b0;
+  count_en = 1'b0;
   case(data_ctrl_current_state)
     WAIT: begin
     end
     STORE: begin
-      msg_valid = 1'b1; 
+      //TODO: Add logic where count_enable & msg_valid would only be updated IF there's a new tdata/message
       for (i = 0; i < TKEEP_WIDTH; i = i + 1) begin
         // {TKEEP_WIDTH{s_tkeep[i]}} ^~ s_tdata[(8i+7):8i]  
         msg_temp[(8*i+7)+:8] = {TKEEP_WIDTH{s_tkeep[i]}} ^~ s_tdata[(8*i+7) +: 8]; 
@@ -92,11 +98,13 @@ always_comb begin
         //set the remaining MSB bits to 0
         msg_temp[8*MAX_MSG_BYTES-1:8*(TKEEP_WIDTH-1)+7] = '0;
       end
-      
+      if (msg_temp != msg_temp_sync) begin
+      	msg_valid = msg_full;
+      	count_en = 1'b1;
+      end
     end
     ERROR: begin
       msg_error = 1'b1;
-      msg_temp = '0;
     end
   endcase
 end
