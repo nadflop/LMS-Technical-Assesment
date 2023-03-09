599a043d53daa0da01ee8a7d72fe8b6742a9e4f3
nad <31370689+nadflop@users.noreply.github.com>
2023-03-10 00:14:11 +0800

    Update axi_slave.sv

--- axi_slave.sv
+++ axi_slave.sv
@@ -4,22 +4,29 @@ module axi_slave (
 	input logic upsizing,
 	input logic s_tvalid,
 	input logic s_tlast,
-	output logic s_tready,
-	output logic clear_msg_count
+	output logic s_tready
 );
 
 //state register for AXI-ST slave FSM
-typedef enum bit [2:0] {IDLE, MODIFY_DATA, WRITE} stateType;
+typedef enum bit [1:0] {IDLE, SEND_STREAM} stateType;
 stateType axi_current_state;
 stateType axi_next_state;
 
+//delay the s_tready by one clock cycle
+//logic s_tready_temp;
+logic s_tready_next = 1'b0;
+
+//assign s_tready = s_tready_temp;
+
 //state assignments for AXI Slave interface
-always_ff @(posedge clk, negedge rst) begin
+always_ff @(posedge clk) begin
   if (!rst) begin
     axi_current_state <= IDLE;
+    s_tready <= 1'b0;
   end
   else begin
     axi_current_state <= axi_next_state;
+    s_tready <= s_tready_next;
   end
 end
 
@@ -29,38 +36,15 @@ always_comb begin
   case(axi_current_state)
     IDLE: begin
       if (s_tvalid && s_tready) begin
-        if (upsizing) begin
-          axi_next_state = MODIFY_DATA;
-        end
-        else begin
-          axi_next_state = WRITE;
-        end
+          axi_next_state = SEND_STREAM;
       end
       else begin
         axi_next_state = axi_current_state;
       end
     end
-    MODIFY_DATA: begin
+    SEND_STREAM: begin
       if (s_tvalid && s_tready) begin
-        if (!s_tlast) begin
-          axi_next_state = axi_current_state;
-        end
-        else begin
-          axi_next_state = WRITE;
-        end
-      end
-      else begin
         axi_next_state = axi_current_state;
-      end
-    end
-    WRITE: begin
-      if (s_tvalid && s_tready) begin
-        if (!s_tlast) begin
-          axi_next_state = axi_current_state;
-        end
-        else begin
-          axi_next_state = IDLE;
-        end
       end
       else begin
         axi_next_state = IDLE;
@@ -71,22 +55,28 @@ end
 
 //output logic for AXI-ST Slave interface
 always_comb begin
-  clear_msg_count = 1'b0;
-  s_tready = 1'b0;
+  s_tready_next = 1'b0;
   case(axi_current_state)
     IDLE: begin
-      s_tready = 1'b1;
-      clear_msg_count = 1'b1;
-    end
-    MODIFY_DATA: begin
-      s_tready = 1'b1;
-    end 
-    WRITE: begin
-      if (s_tlast) begin
-        s_tready = 1'b0;
+      s_tready_next = 1'b1;
+      if (s_tvalid && s_tready) begin
+      	if (s_tlast) begin
+		s_tready_next = 1'b0;
+      	end
+      	else begin
+      		s_tready_next = 1'b1;
+      	end
       end
-      else begin
-        s_tready = 1'b1;
+    end
+    SEND_STREAM: begin
+      s_tready_next = 1'b1;
+      if (s_tvalid && s_tready) begin
+      	if (s_tlast) begin
+		s_tready_next = 1'b0;
+      	end
+      	else begin
+      		s_tready_next = 1'b1;
+      	end
       end
     end
   endcase
